f5cb31d13b80220e9dfe8b5c355cbb811c279b0e
Jessz5 <jessiwu@buffalo.edu>
2025-02-13 18:28:09 -0500

    Update query to calculate average daily ridership for weekdays and weekends

--- project_sql/3_weekday_vs_weekend.sql
+++ project_sql/3_weekday_vs_weekend.sql
@@ -1,10 +1,17 @@
 -- 3. How does weekday ridership compare to weekends?
 
 SELECT
-    CASE
-        WHEN EXTRACT(DOW FROM transit_timestamp) IN (0,6) THEN 'Weekend'
-        ELSE 'Weekday'
-    END AS day_type,
-    SUM(ridership) AS total_ridership
-FROM dec_2024_ridership
+    day_type,
+    AVG(daily_ridership) AS avg_ridership_per_day
+FROM (
+    SELECT
+        DATE(transit_timestamp) AS date,
+        CASE
+            WHEN EXTRACT(DOW FROM transit_timestamp) IN (0,6) THEN 'Weekend'
+            ELSE 'Weekday'
+        END AS day_type,
+        SUM(ridership) AS daily_ridership
+    FROM dec_2024_ridership
+    GROUP BY DATE(transit_timestamp), day_type
+) AS daily_totals
 GROUP BY day_type;
