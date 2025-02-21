0ca4796b5b21f364593cb78fe511b90a633cb062
Jessz5 <jessiwu@buffalo.edu>
2025-02-20 23:01:00 -0500

    Refactor SQL query using CTE for better readability

--- project_sql/3_weekday_vs_weekend.sql
+++ project_sql/3_weekday_vs_weekend.sql
@@ -1,17 +1,19 @@
 -- 3. How does weekday ridership compare to weekends?
 
-SELECT
-    day_type,
-    AVG(daily_ridership) AS avg_ridership_per_day
-FROM (
+WITH daily_totals AS (
     SELECT
         DATE(transit_timestamp) AS date,
         CASE
-            WHEN EXTRACT(DOW FROM transit_timestamp) IN (0,6) THEN 'Weekend'
-            ELSE 'Weekday'
+            WHEN EXTRACT(DOW FROM transit_timestamp) IN (0, 6)
+            THEN 'Weekend' ELSE 'Weekday'
         END AS day_type,
         SUM(ridership) AS daily_ridership
     FROM dec_2024_ridership
-    GROUP BY DATE(transit_timestamp), day_type
-) AS daily_totals
+    GROUP BY date, day_type
+)
+
+SELECT
+    day_type,
+    AVG(daily_ridership) AS avg_ridership_per_day
+FROM daily_totals
 GROUP BY day_type;
